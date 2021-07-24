b9f0631e59d667cc61dcf50a55312c3e7d30ad85
James Kehs <62821048+jimmykehs@users.noreply.github.com>
2021-07-24 15:06:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.use(cors());
 app.use("/api", require("./Routes"));
 
 const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
